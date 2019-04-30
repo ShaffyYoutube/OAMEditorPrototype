9cf672a258c2de8fcc8221290dfdb331fa2491fa
ShaffyYoutube <thedave2cz@gmail.com>
2019-04-30 16:01:25 +0200

    Well First Files

--- OAMEditorPrototype/Form1.Designer.cs
+++ OAMEditorPrototype/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OAMEditorPrototype
 {
-    partial class Form1
+    partial class currentoam
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,13 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupoam = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.oam_list = new System.Windows.Forms.ListBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.offset_string = new System.Windows.Forms.Label();
+            this.offset_select = new System.Windows.Forms.NumericUpDown();
+            this.load = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleBorderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.offset_select)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupoam
+            // 
+            this.groupoam.Location = new System.Drawing.Point(712, 12);
+            this.groupoam.Name = "groupoam";
+            this.groupoam.Size = new System.Drawing.Size(112, 238);
+            this.groupoam.TabIndex = 0;
+            this.groupoam.TabStop = false;
+            this.groupoam.Text = "Group OAMs";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(566, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(140, 238);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Current OAM";
+            // 
+            // oam_list
+            // 
+            this.oam_list.FormattingEnabled = true;
+            this.oam_list.Location = new System.Drawing.Point(440, 12);
+            this.oam_list.Name = "oam_list";
+            this.oam_list.Size = new System.Drawing.Size(120, 238);
+            this.oam_list.TabIndex = 2;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(440, 256);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(58, 23);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(502, 256);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(58, 23);
+            this.delete.TabIndex = 4;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(566, 256);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(58, 23);
+            this.up.TabIndex = 5;
+            this.up.Text = "Up";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(630, 256);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(58, 23);
+            this.down.TabIndex = 6;
+            this.down.Text = "Down";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // offset_string
+            // 
+            this.offset_string.AutoSize = true;
+            this.offset_string.Location = new System.Drawing.Point(440, 287);
+            this.offset_string.Name = "offset_string";
+            this.offset_string.Size = new System.Drawing.Size(38, 13);
+            this.offset_string.TabIndex = 7;
+            this.offset_string.Text = "Offset:";
+            // 
+            // offset_select
+            // 
+            this.offset_select.BackColor = System.Drawing.SystemColors.Window;
+            this.offset_select.Hexadecimal = true;
+            this.offset_select.Location = new System.Drawing.Point(484, 285);
+            this.offset_select.Name = "offset_select";
+            this.offset_select.Size = new System.Drawing.Size(120, 20);
+            this.offset_select.TabIndex = 8;
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(610, 282);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 9;
+            this.load.Text = "Load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(836, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "&Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pasteToolStripMenuItem.Text = "&Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toggleBorderToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // toggleBorderToolStripMenuItem
+            // 
+            this.toggleBorderToolStripMenuItem.Name = "toggleBorderToolStripMenuItem";
+            this.toggleBorderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.toggleBorderToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.toggleBorderToolStripMenuItem.Text = "&Toggle Border";
+            this.toggleBorderToolStripMenuItem.Click += new System.EventHandler(this.toggleBorderToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // currentoam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(836, 578);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.offset_select);
+            this.Controls.Add(this.offset_string);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.oam_list);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupoam);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "currentoam";
+            this.Text = "Rhythm Tengoku OAM Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.offset_select)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupoam;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox oam_list;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Label offset_string;
+        private System.Windows.Forms.NumericUpDown offset_select;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleBorderToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- OAMEditorPrototype/Form1.cs
+++ OAMEditorPrototype/Form1.cs
@@ -10,11 +10,66 @@ using System.Windows.Forms;
 
 namespace OAMEditorPrototype
 {
-    public partial class Form1 : Form
+    public partial class currentoam : Form
     {
-        public Form1()
+        public currentoam()
         {
             InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "TXT (*.txt)|*.txt|OAM (*.oam)|*.oam|All files (*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            ofd.ShowDialog();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void down_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void toggleBorderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented");
         }
     }
 }
